8f150097564b94f0e820abd77adba34eb48ca3cf
Eslam.khder <eslamkhder11@gmail.com>
2025-09-27 23:01:40 +0300

    tasks

--- DataBase lec7/sql files/1. Natural+Join+(Code+Samples).sql.sql
+++ DataBase lec7/sql files/1. Natural+Join+(Code+Samples).sql.sql
@@ -6,4 +6,25 @@ SELECT first_name, last_name, department_name FROM departments NATURAL JOIN empl
 (HR.JHIST_EMP_ID_ST_DATE_PK) 
 
 
-task     trigger 
+SELECT * FROM employees;
+SELECT * FROM departments;
+
+SELECT * FROM employees NATURAL JOIN departments;
+
+DROP TABLE user_907;
+CREATE TABLE user_907 (
+    user_id       NUMBER PRIMARY KEY,
+    username      VARCHAR2(100) NOT NULL UNIQUE,
+    password      VARCHAR2(200) NOT NULL
+);
+
+DROP TABLE user_details_907;
+CREATE TABLE user_details_907 (
+    user_id       NUMBER PRIMARY KEY,
+    first_name    VARCHAR2(100),
+    last_name     VARCHAR2(100),
+    CONSTRAINT fk_user_details_907 FOREIGN KEY (user_id) REFERENCES user_907(user_id)
+);
+
+
+SELECT * FROM user_907 NATURAL JOIN user_details_907;
